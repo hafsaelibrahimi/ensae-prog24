2ee5ec786a3a5474ba3748c8fbdbc2aaa6f02899
hafsaelib <hafsa.elibrahimi@ensae.fr>
2024-03-03 16:45:32 +0000

    git

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -57,8 +57,9 @@ class Grid():
         """
         Checks is the current state of the grid is sorte and returns the answer as a boolean.
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        raise NotImplementedError
+        if self.state == [list(range(i*self.n+1, (i+1)*self.n+1)) for i in range(self.m)] :
+            return True
+        return False
 
     def swap(self, cell1, cell2):
         """
@@ -69,8 +70,13 @@ class Grid():
         cell1, cell2: tuple[int]
             The two cells to swap. They must be in the format (i, j) where i is the line and j the column number of the cell. 
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        raise NotImplementedError
+        (x,y) = cell1
+        (a,b) = cell2
+        if (abs(x-a) == 1 and y==b) or (abs(y-b) == 1 and x==a):
+            self.state[x][y] , self.state[a][b] = self.state[a][b] , self.state[x][y]
+        else:
+            raise ValueError("L'échange entre les cellules spécifiées n'est pas autorisé.")
+        return self.state
 
     def swap_seq(self, cell_pair_list):
         """
@@ -82,8 +88,10 @@ class Grid():
             List of swaps, each swap being a tuple of two cells (each cell being a tuple of integers). 
             So the format should be [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...].
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        raise NotImplementedError
+        for s in cell_pair_list :
+            (cell1 , cell2) = s
+            self.swap(cell1,cell2)
+        return self.state
 
     @classmethod
     def grid_from_file(cls, file_name): 
--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,16 +1,10 @@
 from grid import Grid
 
 class Solver(): 
-    """
-    A solver class, to be implemented.
-    """
+    def __init__(self, m, n, grid):
+        self.m = m
+        self.n = n
+        self.grid = grid
     
     def get_solution(self):
-        """
-        Solves the grid and returns the sequence of swaps at the format 
-        [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
-        """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
-
+        
